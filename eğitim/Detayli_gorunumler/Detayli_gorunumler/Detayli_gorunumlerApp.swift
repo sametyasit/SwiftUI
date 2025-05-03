0e54f3b4d2961b99be3888d090fcac3d1a5d8d58
SametYASIT <120249031+SametYASIT@users.noreply.github.com>
2025-05-03 17:43:20 +0300

    .

---
+++ eğitim/Detayli_gorunumler/Detayli_gorunumler/Detayli_gorunumlerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Detayli_gorunumlerApp.swift
+//  Detayli_gorunumler
+//
+//  Created by Samet on 03/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct Detayli_gorunumlerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
