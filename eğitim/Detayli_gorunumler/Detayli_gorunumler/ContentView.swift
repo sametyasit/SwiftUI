4377ab8da998bd9392dca7282e23e3a47eec108c
SametYASIT <120249031+SametYASIT@users.noreply.github.com>
2025-05-03 18:22:40 +0300

    .

--- eğitim/Detayli_gorunumler/Detayli_gorunumler/ContentView.swift
+++ eğitim/Detayli_gorunumler/Detayli_gorunumler/ContentView.swift
@@ -10,10 +10,32 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
+            Image("cursor")
+                .resizable()
+                .aspectRatio(contentMode:.fit)
+                .frame(width: UIScreen.main.bounds.width * 0.9, height: UIScreen.main.bounds.height * 0.26 , alignment: .center)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.purple,lineWidth : 4))
+                .shadow(radius: 100)
+                .padding()
+            
+            Image("cursor2")
+                .resizable()
+                .aspectRatio(contentMode:.fit)
+                .frame(width: UIScreen.main.bounds.width * 0.9, height: UIScreen.main.bounds.height * 0.26 , alignment: .center)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.purple,lineWidth : 4))
+                .shadow(radius: 50)
+            
+            Text("Hello, Cursor")
+                .foregroundStyle(.blue)
+                .fontWeight(.heavy)
+                .fontDesign(.monospaced)
+                .font(.largeTitle)
+                .padding()
+            
             Text("Hello, world!")
+                .padding()
         }
         .padding()
     }
